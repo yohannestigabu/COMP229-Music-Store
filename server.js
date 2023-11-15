136cb8370b96c38fddcd46705c4e3457c9da90f1
Zoe <114508304+zgawlak@users.noreply.github.com>
2023-11-14 21:55:49 -0500

    Change static filepath

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const app = express();
 const assetsRouter = require("./server/assets-router");
 app.use("/src", assetsRouter);
-app.use("/", express.static(path.join(__dirname, "public")));
+app.use("/", express.static(path.join(__dirname, "music-store/public")));
 app.get("/api/v1", (req, res) => {
     res.json({
         project: "React and Express Boilerplate",
